a25ad56f7f63344d982045f4c5bf90d3760f06e1
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-21 15:01:12 +0300

    Created Image model

--- social_network/models.py
+++ social_network/models.py
@@ -1,6 +1,10 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
 
 
 class Post(models.Model):
@@ -46,3 +50,18 @@ class Tag(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+def create_image_path(instance, filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.user.username)}-{uuid.uuid4()}{extension}"
+    return os.path.join(f"posts/post{instance.pk}", filename)
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to=create_image_path)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="images"
+    )
