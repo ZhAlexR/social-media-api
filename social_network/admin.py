2f2bee23a92ac6b16eeff553fc4a900516bc63f3
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-21 15:01:31 +0300

    Registered Image model

--- social_network/admin.py
+++ social_network/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from social_network.models import Post, Like, Comment, Tag
+from social_network.models import Post, Like, Comment, Tag, Image
 
 admin.site.register(Post)
 admin.site.register(Like)
 admin.site.register(Comment)
 admin.site.register(Tag)
+admin.site.register(Image)
