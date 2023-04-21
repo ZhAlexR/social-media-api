e00f524839d818c36c18dbb15e736fc00cf90ab7
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-21 18:11:52 +0300

    Implemented CreateUserView and ProfileViewSet

--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from user.models import Profile
+from user.serializers import ProfileSerializer, UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ProfileViewSet(ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
