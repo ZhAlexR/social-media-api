bb7117dfd6e727769982ac2c32ad0b6f5f42daf1
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-21 13:20:29 +0300

    Creates User and Profile models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    photo = models.ImageField(upload_to=f"profiles/{user.username}")
+    bio = models.TextField()
+
+    def __str__(self) -> str:
+        return f"profile{self.id} ({self.user.username})"
