a1c4d91d26338b3a137584ead5240a56d1cdc313
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-21 13:51:39 +0300

    Implemented path generation logic for "photo" field

--- user/models.py
+++ user/models.py
@@ -1,10 +1,20 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.text import slugify
 
 
 class User(AbstractUser):
     pass
+
+
+def create_photo_path(instance, filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.user.username)}-{uuid.uuid4()}{extension}"
+    return os.path.join(f"profiles/{instance.user.username}{instance.user.pk}", filename)
 
 
 class Profile(models.Model):
@@ -13,8 +23,9 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         related_name="profile"
     )
-    photo = models.ImageField(upload_to=f"profiles/{user.username}")
+    photo = models.ImageField(upload_to=create_photo_path,
+                              null=True, blank=True)
     bio = models.TextField()
 
     def __str__(self) -> str:
-        return f"profile{self.id} ({self.user.username})"
+        return f"profile{self.id} ({self.user})"
