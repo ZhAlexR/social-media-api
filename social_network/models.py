0f41133bd10a7dbc06c7510684f67090367cec82
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-21 14:32:33 +0300

    Created models for "social_network" app

--- social_network/models.py
+++ social_network/models.py
@@ -1,3 +1,48 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.title[:10]} by {self.owner.username}"
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=255)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.text[:10]} by {self.owner.username}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+    post = models.ManyToManyField(Post, related_name="tags")
+
+    def __str__(self) -> str:
+        return self.name
