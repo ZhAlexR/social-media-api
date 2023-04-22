6fe67c1f36294358a692725fb1eab1ab4f5a9014
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-22 14:15:59 +0300

    Added route for PostViewSet

--- social_network/urls.py
+++ social_network/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from social_network.views import PostListView
+from social_network.views import PostViewSet
 from user.views import ProfileViewSet
 
 app_name = "social_network"
 
 route = routers.DefaultRouter()
 route.register("profiles", ProfileViewSet, basename="profile")
+route.register("posts", PostViewSet, basename="post")
 
 urlpatterns = [
-    path("posts/", PostListView.as_view(), name="post-list"),
     path("", include(route.urls)),
 ]
