e0a2922e45d73c78f26c69305c112aa8d15d051b
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-22 18:16:21 +0300

    Added Image serializer

--- social_network/serializers.py
+++ social_network/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 
-from social_network.models import Post
+from social_network.models import Post, Image
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ["id", "image"]
+
+    def create(self, validated_data):
+        post = self.context.get("post")
+        image = validated_data.get("image")
+        return Image.objects.create(image=image, post=post)
 
 
 class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ["id", "title", "images", "text", "created_at", "updated_at", "owner"]
+        fields = ["id", "title", "text", "created_at", "updated_at", "owner"]
         read_only_fields = ["id", "created_at", "updated_ad", "owner"]
