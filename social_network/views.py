f1ac150e955facc6d528098f3fdd82a0c1cd0a8a
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-22 14:15:08 +0300

    Made post owner as current user

--- social_network/views.py
+++ social_network/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import viewsets
 
 from social_network.models import Post
+from social_network.permissions import IsOwnerOrReadOnly
 from social_network.serializers import PostSerializer
 
 
-class PostListView(ListCreateAPIView):
+class PostViewSet(
+    viewsets.ModelViewSet
+):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save()
