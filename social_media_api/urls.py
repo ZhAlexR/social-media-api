e4b8274070641555e0125bb1ae87c7388629bfe1
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-23 16:18:26 +0300

    Added path to "swagger" documentation

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +27,10 @@ urlpatterns = [
         include("social_network.urls", namespace="social_network"),
     ),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
