889832101e583085e8a2971dc02e9644f4607545
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-23 09:33:27 +0300

    Added path for reactions

--- social_network/urls.py
+++ social_network/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from social_network.views import PostViewSet, CreateTagView
+from social_network.views import PostViewSet, CreateTagView, toggle_like, toggle_dislike
 from user.views import ProfileViewSet
 
 app_name = "social_network"
@@ -12,5 +12,7 @@ route.register("posts", PostViewSet, basename="post")
 
 urlpatterns = [
     path("tags/create", CreateTagView.as_view(), name="tag-create"),
+    path("toggle-like/post/<int:pk>/", toggle_like, name="toggle-like"),
+    path("toggle-dislike/post/<int:pk>/", toggle_dislike, name="toggle-dislike"),
     path("", include(route.urls)),
 ]
